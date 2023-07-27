5781d2e85f2136a6c16d60128813bdbc4276004f
roubincode <liaoxiangnign@taikr.com>
2023-07-27 20:12:57 +0800

    add do

---
+++ Skill/Skill.cs
@@ -0,0 +1,17 @@
+void Create(){
+    // asfas
+    var entity = MongoHelper.Instance.Clone(this);
+    entity.Deserialize(Scene, true);
+
+    if(){
+      aaa
+    }else if(){
+      bbb
+    }else if(){
+      ccc
+    }
+}
+
+public void Do(){
+    ///...
+}
